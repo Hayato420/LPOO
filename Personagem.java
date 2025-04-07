1d24d40ec89f09394480ef61f9a5b58515de6572
Hayato420 <mjsa@poli.br>
2025-04-07 11:42:42 -0300

    Update Personagem.java

--- Personagem.java
+++ Personagem.java
@@ -9,27 +9,54 @@ public class Personagem{
         private int sede;
         private int energia;
         private int sanidade;
-        private List<Item> inventario = new ArrayList<>();
-        private Ambiente ambienteAtual;
+        /*private List<Item> inventario = new ArrayList<>();*/
+        private Ambiente localizacao;
 
-        public Personagem(String nome, Ambiente ambienteInicial){
+        public Personagem(String nome, Ambiente ambienteInicialAleatorio){
             this.nome = nome;
-            this.vida = 100;
+            this.vida = 150;
             this.energia = 100;
-            this.fome = 0;
+            this.fome = 100;
             this.sanidade = 100;
-            this.inventario = new ArrayList<>();
-            this.ambienteAtual = ambienteInicial;
+            /*this.inventario = new ArrayList<>();*/
+            this.localizacao = ambienteInicialAleatorio;
         }
 
-        public void usarHabilidade(){};
+        /* public void usarHabilidade(){}; SERA ABSTRATO, CADA SUBCLASSE DEFINIRA SUA MODO DE USO */
 
-        public Ambiente getAmbienteAtual() {
-            return ambienteAtual;
+
+        public String getNome(){
+            return this.nome;
         }
 
-        public void setAmbienteAtual(Ambiente novoAmbiente) {
-            this.ambienteAtual = novoAmbiente;
+        public Ambiente getLocalizacao() {
+            return localizacao;
+        }
+
+        public void setLocalizacao(Ambiente novaLocalizacao){
+            this.localizacao = novaLocalizacao;
+        }
+
+        public int getVida() {
+            return this.vida;
+        }
+
+        public void adicionarVida(int quantidade){
+            this.vida += quantidade;
+            if (this.vida > 150){
+                this.vida = 150;
+            }
+        }
+
+        public void perderVida(int quantidade){
+            this.vida -= quantidade;
+            if(this.vida <=0){
+                System.out.print("Seus pontos de vida acabaram. Fim de jogo.");
+            }
+        }
+
+        public int getEnergia(){
+            return this.energia;
         }
 
         public void adicionarEnergia(int quantidade){
@@ -45,8 +72,63 @@ public class Personagem{
                 System.out.print("Sua energia acabou. Fim de jogo.");
             }
         }
-    }
 
+        public int getFome(){
+            return this.fome;
+        }
+
+        public void adicionarFome(int quantidade){
+            this.fome += quantidade;
+            if(this.fome > 100){
+                this.fome = 100;
+            }
+        }
+
+        public void perderFome(int quantidade){
+            this.fome -= quantidade;
+            if(this.fome <= 0){
+                System.out.print("Sua fome zerou. Fim de jogo.");
+            }
+        }
+
+        public int getSede(){
+            return this.fome;
+        }
+
+        public void adicionarSede(int quantidade){
+            this.fome += quantidade;
+            if(this.fome > 100){
+                this.fome = 100;
+            }
+        }
+
+        public void perderSede(int quantidade){
+            this.fome -= quantidade;
+            if(this.fome <= 0){
+                System.out.print("Sua fome zerou. Fim de jogo.");
+            }
+        }
+
+        public int getSanidade(){
+            return this.sanidade;
+        }
+
+        public void adicionarSanidade(int quantidade){
+            this.sanidade += quantidade;
+            if (this.sanidade > 100){
+                this.sanidade = 100;
+            }
+        }
+
+        public void perderSanidade (int quantidade) {
+            this.sanidade -= quantidade;
+            if(this.sanidade <= 0){
+                System.out.print("Você ensandeceu. Fim de jogo.");
+            }
+        }
+}
+
+/*
 class Rastreador extends Personagem{
     public Rastreador (String nome, Ambiente ambienteInicial, int energia){
         super(nome, ambienteInicial);   
@@ -60,3 +142,4 @@ class Rastreador extends Personagem{
         }
     }
 }
+*/
