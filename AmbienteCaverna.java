86584d8d526ee10bedc5be4f186cf8628a444f03
Miguel-Masiero <miguelmasiero1@gmail.com>
2025-04-22 11:56:02 -0300

    Create AmbienteCaverna.java

---
+++ AmbienteCaverna.java
@@ -0,0 +1,14 @@
+public class AmbienteCaverna extends Ambiente{
+    //construtor da subclasse
+    public AmbienteCaverna(){
+        super(
+            "Caverna",
+            "Um grande túnel que passa por baixo da terra, escondido do sol e fácil de se perder",
+            3,
+            Clima.UMIDO,
+            Temperatura.NEUTRO
+        );
+        
+    }
+
+}
