e2d90047023d20e211652adb9bc94ca064adb056
Miguel-Masiero <miguelmasiero1@gmail.com>
2025-04-22 11:55:14 -0300

    Create AmbienteDeserto.java

---
+++ AmbienteDeserto.java
@@ -0,0 +1,14 @@
+public class AmbienteDeserto extends Ambiente{
+    //construtor da subclasse
+    public AmbienteDeserto(){
+        super(
+            "Deserto",
+        "Uma região extremamente seca e quente, com poucos recursos, onde o ambiente é o maior perigo",
+        4,
+        Clima.SECO,
+        Temperatura.QUENTE
+        );
+        
+    }
+
+}
