ce52ca624a997302eb1f1dca3a2332354e8efa23
Miguel-Masiero <miguelmasiero1@gmail.com>
2025-04-22 20:43:22 -0300

    Update Arma.java

--- Arma.java
+++ Arma.java
@@ -1,40 +1,40 @@
 public class Arma extends Item{
+//MUNICAO SERA ITENS NO INVENTARIO, ARCO->FLECHA, PISTOLA->BALA, ETC.
     private tipoArma tipo;
-    private enum tipoArma {
+    public enum tipoArma{
         corpoACorpo,
         aDistancia
     }
+    private qualArma qual;
+    public enum qualArma{
+        ESPADA,
+        ARCO,
+        PISTOLA
+    }
     private int dano;
     private int alcance;
-    
-    public Arma(String nome, String descricao, int peso, int durabilidade, tipoArma tipo, int dano, int alcance) {
+
+    public Arma(String nome, String descricao, int peso, int durabilidade, tipoArma tipo, qualArma qual, int dano, int alcance){
         super(nome, descricao, peso, durabilidade);
         this.tipo = tipo;
+        this.qual = qual;
         this.dano = dano;
         this.alcance = alcance;
     }
 
-    public tipoArma getTipo() {
-        return tipo;
+    public tipoArma getTipo(){
+        return this.tipo;
     }
 
-    public int getDano() {
-        return dano;
+    public qualArma getQual(){
+        return this.qual;
     }
 
-    public int getAlcance() {
-        return alcance;
+    public int getDano(){
+        return this.dano;
     }
 
-    public static Arma gerarEspada() {
-        return new Arma("Espada", "Uma espada afiada, mas de curta distancia.", 3, 10, tipoArma.corpoACorpo, 25, 1);
-    }
-
-    public static Arma gerarArco() {
-        return new Arma("Arco", "Um arco de longa distancia.", 2, 10, tipoArma.aDistancia, 15, 3);
-    }
-
-    public static Arma gerarPistola() {
-        return new Arma("Pistola", "Uma espada afiada, mas de curta distancia.", 2, 10, tipoArma.corpoACorpo, 25, 1);
+    public int getAlcance(){
+        return this.alcance;
     }
 }
