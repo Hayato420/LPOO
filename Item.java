4d26c2e7626fb3b4ddfeefa13c1542a2f08f4e51
Miguel-Masiero <miguelmasiero1@gmail.com>
2025-04-22 20:42:20 -0300

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,16 +1,47 @@
+import java.util.UUID;
+
 public class Item{
     private String nome;
+    private String ID;
     private String descricao;
     private int peso;
     private int durabilidade;
-    public void usar(){}
-    public void beber(){}
-    public void consumir(){}
+    public void usar(Personagem jogador){}
 
     public Item(String nome, String descricao, int peso, int durabilidade){
         this.nome = nome;
+        this.ID = UUID.randomUUID().toString();
         this.descricao = descricao;
         this.peso = peso;
         this.durabilidade = durabilidade;
     }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getID(){
+        return this.ID;
+    }
+
+    public String getDescricao(){
+        return this.descricao;
+    }
+
+    public int getPeso(){
+        return this.peso;
+    }
+
+    public int getDurabilidade(){
+        return this.durabilidade;
+    }
+
+    public void perderDurabilidade(Inventario inventario){
+        this.durabilidade -= 1;
+        if(this.durabilidade <= 0){
+            System.out.println("A ferramenta quebrou.");
+            inventario.removerItem(this.ID);
+        }
+    }
+
 }
