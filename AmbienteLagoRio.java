84e04018410e099042119a8c9aa24ce78fc7cd9d
Miguel-Masiero <miguelmasiero1@gmail.com>
2025-04-22 19:38:29 -0300

    Create AmbienteLagoRio.java

---
+++ AmbienteLagoRio.java
@@ -0,0 +1,12 @@
+public class AmbienteLagoRio extends Ambiente{
+    //construtor da subclasse
+    public AmbienteLagoRio(){
+        super(
+            "Zona alagada",
+            "Uma próspera região, rica em água e alimentos, mas pouca matéria prima",
+            3,
+            Clima.UMIDO,
+            Temperatura.NEUTRO
+        );
+    }
+}
