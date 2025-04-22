be4b91df217e5ba8f3923c26f22c2f6383129bb2
Miguel-Masiero <miguelmasiero1@gmail.com>
2025-04-22 15:56:08 -0300

    Add files via upload

---
+++ AmbienteRuinas.java
@@ -0,0 +1,12 @@
+public class AmbienteRuinas extends Ambiente{
+    //construtor da subclasse
+    public AmbienteRuinas(){
+        super(
+            "Ruinas",
+            "Perdidas e esquecidas pelo tempo, estas estruturas podem ainda ter algo útil...",
+            2,
+            Clima.AMENO,
+            Temperatura.NEUTRO
+        );
+    }
+}
