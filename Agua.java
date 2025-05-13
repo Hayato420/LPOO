9eaac3ed5207161bf4253257a3538eaf39a5e72c
Miguel-Masiero <miguelmasiero1@gmail.com>
2025-05-13 19:18:04 -0300

    Update Agua.java

--- Agua.java
+++ Agua.java
@@ -2,14 +2,31 @@ public class Agua extends Item{
     private boolean pureza;
     private int volume;
 
-    public void diminuirVolume(){
+    public Agua(String nome, String descricao, int peso, boolean pureza, int volume){
+        super(nome, descricao, peso, 1);
+        this.pureza = pureza;
+        this.volume = volume;
+    }
+
+    public void diminuirVolume(Inventario inventario){
         this.volume -= 1;
-        if(this.volume < 0){
-            //REMOVER O ITEM DO INVENTARIO, OU DEIXAR A GARRAFA MAS VAZIA, O QUE SERIA MAIS COMPLICADO POIS TERIA QUE TER O ITEM GARRAFA E SO PODER ENCHER EM ALGUMA FONTE PROXIMA
+        if(this.volume <= 0){
+            inventario.removerItem(this.getID());
         }
     }
-    public void usar(Personagem jogador){
+    
+    public void beber(Personagem jogador, Inventario inventario){
         jogador.adicionarSede(10);
-        this.diminuirVolume();
+        this.diminuirVolume(inventario);
+    }
+
+    public boolean getPureza(){
+        return this.pureza;
+    }
+
+    public void purificar(){
+        if(this.pureza == false){
+            this.pureza = true;
+        }
     }
 }
