86f0535aeec1dcf3611ceb30f01ef780688db588
Miguel-Masiero <miguelmasiero1@gmail.com>
2025-05-13 19:23:21 -0300

    Create Isqueiro.java

---
+++ Isqueiro.java
@@ -0,0 +1,10 @@
+public class Isqueiro extends Ferramenta{
+
+    public Isqueiro(String nome, String descricao, int peso, Material material1, Material material2) {
+        super(nome, descricao, peso, material1, material2);
+    }
+
+    public void exercerUso(Personagem jogador){
+        //+incendiar e talvez iluminar
+    }
+}
