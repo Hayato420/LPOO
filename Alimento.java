037ae7f093611ceac7e6c70317698664377d1665
Miguel-Masiero <miguelmasiero1@gmail.com>
2025-05-13 19:18:41 -0300

    Update Alimento.java

--- Alimento.java
+++ Alimento.java
@@ -1,31 +1,39 @@
 public class Alimento extends Item{
-    private int valorNutricional;
-    private enum tipoAlimento{
-        FRUTA{
-            @Override
-            public void usar(Personagem jogador){
-                jogador.adicionarFome(10);
-            }
-        },
-        CARNE{
-            @Override
-            public void usar(Personagem jogador){
-                jogador.adicionarFome(10);
-            }
-        },
-        ENLATADO{
-            @Override
-            public void usar(Personagem jogador){
-                jogador.adicionarFome(10);
-            }
-        },
-        FARINACEOS{
-            @Override
-            public void usar(Personagem jogador){
-                jogador.adicionarFome(10);
-            }
+    private final String tipoAlimento;
+    private int prazoDeValidade;
+    private boolean apodrecido;
+    private final int valorNutricional;
+
+    public Alimento(String nome, String descricao, String tipoAlimento, int peso, int prazoDeValidade, int valorNutricional){
+        super(nome, descricao, peso, 1);
+        this.tipoAlimento = tipoAlimento;
+        this.prazoDeValidade = prazoDeValidade;
+        this.apodrecido = false;
+        this.valorNutricional = valorNutricional;
+    }
+
+    public String getTipoAlimento(){
+        return this.tipoAlimento;
+    }
+
+    public void usar(Personagem jogador, Inventario inventario){
+        if(this.apodrecido == false){
+            jogador.adicionarFome(this.valorNutricional);
+            inventario.removerItem(this.getID());
+        }
+        else{
+            jogador.adicionarFome(this.valorNutricional/2);
+            jogador.perderVida(15);
+            inventario.removerItem(this.getID());
         }
     }
-    private int prazoDeValidade;
-    private boolean apodrecido = false;
+    
+    public void apodrecer(){
+        this.prazoDeValidade -=1;
+        if(this.prazoDeValidade <= 0){
+            this.apodrecido = true;
+            System.out.printf("O alimento %s apodreceu.\n", this.getNome());
+        }
+    }
+    
 }
