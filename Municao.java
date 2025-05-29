46e3a0546dfba373c188719da73cca713f513219
Hayato420 <mjsa@poli.br>
2025-05-28 22:53:27 -0300

    Update Municao.java

--- Municao.java
+++ Municao.java
@@ -1,7 +1,13 @@
 public class Municao extends Item{
     private int quantia;
-    public Municao(String nome, String descricao, int peso, int durabilidade, GeradorDeID geradorDeID, int quantia){
+    private final Material material1;
+    private final Material material2;
+    
+    public Municao(String nome, String descricao, int peso, int durabilidade, int quantia,
+                   Material material1, Material material2, GeradorDeID geradorDeID){
         super(nome, descricao, peso, durabilidade, geradorDeID);
+        this.material1 = material1;
+        this.material2 = material2;
         this.quantidade = quantia;
     }
     //QUANTIA
@@ -16,5 +22,18 @@ public class Municao extends Item{
     }
     public void aumentarQuantia(int quantia){
         this.quantia += quantia;
+    }
+
+    public void usar(Personagem jogador){
+        System.out.println("Usavel em outro item.");
+    }
+
+    public void feitaDe(){
+        if(this.material1.getTipoDeMaterial() == this.material2.getTipoDeMaterial()){
+            System.out.println("Feita de " + material1.getNome() + ".");
+        }
+        else{
+        System.out.println("Feita de " + material1.getNome() + " e " + material2.getNome() + ".");
+        }
     }
 }
