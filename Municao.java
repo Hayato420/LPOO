be7ffdd0cf315f4e46e4ac3bef71b001727b2490
Hayato420 <mjsa@poli.br>
2025-05-28 23:32:45 -0300

    Update Municao.java

--- Municao.java
+++ Municao.java
@@ -1,36 +1,58 @@
 public class Municao extends Item{
     private int quantiaAtual;
-    private final int quantiaMax;
+    //quantia maxima definida em cada item
     private final Material material1;
     private final Material material2;
     
-    public Municao(String nome, String descricao, int peso, int durabilidade, int quantiaMax,
+    public Municao(String nome, String descricao, int peso, int durabilidade, int quantiaAtual,
                    Material material1, Material material2, GeradorDeID geradorDeID){
         super(nome, descricao, peso, durabilidade, geradorDeID);
         this.material1 = material1;
         this.material2 = material2;
-        this.quantiaAtual = quantiaMax;
-        this.quantiaMax = quantiaMax;
+        this.quantiaAtual = quantiaAtual;//NAO FILTRA DE A QUANTIA ATUAL PASSA A MAXIMA, CUIDADO DEVE SER FEITO EM QUEM CHAMAR
     }
     //QUANTIA
-    public void getQuantia(){
-        return this.quantia;
+    public void getQuantiaAtual(){
+        return this.quantiaAtual;
     }
-    public void setQuantia(int quantia){
-        this.quantia = quantia;
+
+    public void setQuantiaAtual(int quantia){
+        this.quantiaAtual = quantia;
     }
     
+    public void getQuantiaMax(){
+        return this.quantiaMax;
+    }
+    
+    public void setQuantiaMax(int quantia){
+        this.quantiaMax = quantia;
+    }
+    //USO
     public void diminuirQuantia(Personagem jogador, int quantia){
         this.quantiaAtual -= quantia;
         if(this.quantiaAtual <= 0){
             jogador.getInventario().removerItem(this.getID());
         }
     }
-    public void aumentarQuantia(Personagem jogador, int quantia){
-        this.quantiaAtual += quantia;
-        if(this.quantiaAtual > this.quantiaMax){this.quantiaAtual = this.quantiaMax;}
+    //RECARGA SE ADICIONAR MAIS AO INVENTARIO 
+    public abstract void aumentarQuantia(Personagem jogador, int quantia){ //getQuantiaAtual() do novo item passado como parâmetro
+        for (Item item : jogador.getInventario().getItens()){
+            if (item instanceof XXXX){
+                XXXX xxxx = (XXXX) item;
+                int complemento = xxxx.getQuantiaMax() - xxxx.getQuantiaAtual();
+                int qtdRestante = Math.min(quantia, complemento);
+                xxxx.setQuantiaAtual(xxxx.getQuantiaAtual() + qtdRestante);
+                quantia -= qtdRestante;
+                if (quantia == 0) break;
+            }
+        }
+    
+        if (quantia > 0){
+            jogador.getInventario().adicionarItem(new XXXX(jogador.getGeradorDeID(), quantia));
+        }
     }
 
+    //OVERRIDE OBRIGATORIO DO ABSTRATO, QUANDO FOR USAR -> UTILIZAR diminuirQuantia()
     public void usar(Personagem jogador){
         System.out.println("Usavel em outro item.");
     }
