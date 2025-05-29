81282da19bb08a9446f12ce1aa2d8ffc35bc2e14
Hayato420 <mjsa@poli.br>
2025-05-29 10:34:51 -0300

    Update Flechas.java

--- Flechas.java
+++ Flechas.java
@@ -1,5 +1,27 @@
 public class Flechas extends Municao{
-    public Flechas(GeradorDeID geradorDeID, int quantia){
-        super("Flechas", "Pontudas.", 1, 1, geradorDeID, quantia);
+    private final int quantiaMax;
+
+    public Flechas(int quantiaAtual, Material material1, Material material2, GeradorDeID geradorDeID){
+        super("Flechas", "Balas primitivas !", 1, 1, quantiaAtual, material1, material2, geradorDeID);
+        this.quantiaMax = 4;
+    }
+
+    @Override
+    public void aumentarQuantia(Personagem jogador, int quantiaAdicionada){ //getQuantiaAtual() do novo item passado como parâmetro
+        if (quantiaAdicionada <= 0) return;
+        for (Item item : jogador.getInventario().getItens()){
+            if (item instanceof Flechas){
+                Flechas flechas = (Flechas) item;
+                int complemento = flechas.getQuantiaMax() - flechas.getQuantiaAtual();
+                int qtdRestante = Math.min(quantiaAdicionada, complemento);
+                flechas.setQuantiaAtual(flechas.getQuantiaAtual() + qtdRestante);
+                quantiaAdicionada -= qtdRestante;
+                if (quantiaAdicionada == 0) break;
+            }
+        }
+    }
+
+    public int getQuantiaMax(){
+        return this.quantiaMax;
     }
 }
