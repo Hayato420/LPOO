f5dd5095c377d7674e9dc2ef286e2ae923a05a30
Hayato420 <mjsa@poli.br>
2025-05-29 10:41:14 -0300

    Update FluidoDeIsqueiro.java

--- FluidoDeIsqueiro.java
+++ FluidoDeIsqueiro.java
@@ -1,5 +1,27 @@
 public class FluidoDeIsqueiro extends Municao{
-    public FluidoDeIsqueiro(GeradorDeID geradorDeID, int quantia){
-        super("FluidoDeIsqueiro", "Fogo.", 1, 1, geradorDeID, quantia);
+    private final int quantiaMax;
+
+    public FluidoDeIsqueiro(int quantiaAtual, Material material1, Material material2, GeradorDeID geradorDeID){
+        super("FluidoDeIsqueiro", "Nao beba isso, tem cheiro de petroleo.", 1, 1, quantiaAtual, material1, material2, geradorDeID);
+        this.quantiaMax = 20;
+    }
+
+    @Override
+    public void aumentarQuantia(Personagem jogador, int quantiaAdicionada){ //getQuantiaAtual() do novo item passado como parâmetro
+        if (quantiaAdicionada <= 0) return;
+        for (Item item : jogador.getInventario().getItens()){
+            if (item instanceof FluidoDeIsqueiro){
+                FluidoDeIsqueiro fluidoDeIsqueiro = (FluidoDeIsqueiro) item;
+                int complemento = fluidoDeIsqueiro.getQuantiaMax() - fluidoDeIsqueiro.getQuantiaAtual();
+                int qtdRestante = Math.min(quantiaAdicionada, complemento);
+                fluidoDeIsqueiro.setQuantiaAtual(fluidoDeIsqueiro.getQuantiaAtual() + qtdRestante);
+                quantiaAdicionada -= qtdRestante;
+                if (quantiaAdicionada == 0) break;
+            }
+        }
+    }
+
+    public int getQuantiaMax(){
+        return this.quantiaMax;
     }
 }
