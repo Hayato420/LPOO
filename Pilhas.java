75ebfbf804839e70ea734adbccf0d257dcd46bb1
Hayato420 <mjsa@poli.br>
2025-05-29 10:36:58 -0300

    Update Pilhas.java

--- Pilhas.java
+++ Pilhas.java
@@ -1,5 +1,27 @@
 public class Pilhas extends Municao{
-    public Pilhas(GeradorDeID geradorDeID, int quantia){
-        super("Pilhas", "Oxirreducao.", 1, 1, geradorDeID, quantia);
+    private final int quantiaMax;
+
+    public Pilhas(int quantiaAtual, Material material1, Material material2, GeradorDeID geradorDeID){
+        super("Pilhas", "Oxirreducao do balacobaco !", 1, 1, quantiaAtual, material1, material2, geradorDeID);
+        this.quantiaMax = 20;
+    }
+
+    @Override
+    public void aumentarQuantia(Personagem jogador, int quantiaAdicionada){ //getQuantiaAtual() do novo item passado como parâmetro
+        if (quantiaAdicionada <= 0) return;
+        for (Item item : jogador.getInventario().getItens()){
+            if (item instanceof Pilhas){
+                Pilhas pilhas = (Pilhas) item;
+                int complemento = pilhas.getQuantiaMax() - pilhas.getQuantiaAtual();
+                int qtdRestante = Math.min(quantiaAdicionada, complemento);
+                pilhas.setQuantiaAtual(pilhas.getQuantiaAtual() + qtdRestante);
+                quantiaAdicionada -= qtdRestante;
+                if (quantiaAdicionada == 0) break;
+            }
+        }
+    }
+
+    public int getQuantiaMax(){
+        return this.quantiaMax;
     }
 }
