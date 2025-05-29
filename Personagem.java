87779d81442e025f3cd6792fac502bcccd4aced8
Hayato420 <mjsa@poli.br>
2025-05-29 08:34:06 -0300

    Atualizar o Personagem.java

--- Personagem.java
+++ Personagem.java
@@ -336,6 +336,18 @@ public abstract class Personagem{
         this.recursosProximos = recursosProximos;
     }
 
+    public void exibirRecursosProximos(){
+        System.out.println("Recursos coletaveis: ");
+        if (this.getRecursosProximos().isEmpty()){
+           System.out.println("Nenhum recurso coletavel por perto.");
+           return;
+        } else{
+        for(Item item : this.getRecursosProximos()){
+              System.out.println(item.getNome() + ": " + item.getID());
+             }
+        }
+    }
+
     public void exibirItens(){
         this.getInventario().exibirItens();
     }
